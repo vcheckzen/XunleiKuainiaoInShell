cc3a1925c5148dd0e86ff7fae9790b64e041c6af
vcheckzen <vcheckzen@outlook.com>
2019-04-19 18:48:51 +0800

    Fix bash directory

--- browser.sh
+++ browser.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
 HEADER="User-Agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/73.0.3683.86 Safari/537.36"
 CONNECTION_TIME="15"
